aa418a0c377995a1f292f1250d4449789ea5d3b0
Robson Braga de Queiroz <rbq.dev@gmail.com>
2018-10-07 21:24:52 -0300

    Update webpack.config.js - Add entry jsx file - Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const mode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     mode: mode ? 'development' : 'production',
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
@@ -18,14 +18,31 @@ module.exports = {
             template: './src/index.html'
         })
     ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    // {
+                    //     loader: MiniCssExtractPlugin.loader,
+                    //     options: {
+                    //         publicPath: path.resolve(__dirname, '/')
+                    //     }
+                    // },
+                    "style-loader",
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
             },
         ]
     }
